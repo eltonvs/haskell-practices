badf49eed266f3151d1bca0a080064dd3e620517
Elton Viana <eltonvs@outlook.com>
2018-11-28 00:44:25 -0300

    Add some unsolved exercises

---
+++ IMD0211/ExRat.hs
@@ -0,0 +1,38 @@
+module ExRat
+    ( rat
+    , (//)
+    , denominator
+    , numerator
+    ) where
+
+data Rat = Undefined
+
+instance Show Rat where
+    show = undefined
+
+instance Eq Rat where
+    (==) = undefined
+
+instance Num Rat where
+    (+) = undefined
+    (*) = undefined
+    negate = undefined
+    abs = undefined
+    signum = undefined
+    fromInteger = undefined
+
+instance Ord Rat where
+    compare = undefined
+
+rat :: Integer -> Integer -> Rat
+rat = undefined
+
+(//) :: Rat -> Rat -> Rat
+(//) = undefined
+
+denominator :: Rat -> Integer
+denominator = undefined
+
+numerator :: Rat -> Integer
+numerator = undefined
+
